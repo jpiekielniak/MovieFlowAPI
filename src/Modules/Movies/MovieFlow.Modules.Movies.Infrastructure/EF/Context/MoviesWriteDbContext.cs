b4af367542f5d86e7d01b1981bfee51a5b1af393
jpiekielniak <jakubpiekielniak123@gmail.com>
2024-02-04 12:22:53 +0100

    add: override method to DbContext

--- src/Modules/Movies/MovieFlow.Modules.Movies.Infrastructure/EF/Context/MoviesWriteDbContext.cs
+++ src/Modules/Movies/MovieFlow.Modules.Movies.Infrastructure/EF/Context/MoviesWriteDbContext.cs
@@ -1,22 +1,42 @@
 using Microsoft.EntityFrameworkCore;
 using MovieFlow.Modules.Movies.Core.Movies.Entities;
 using MovieFlow.Modules.Movies.Infrastructure.EF.Movies.Configurations.Write;
+using MovieFlow.Shared.Abstractions.Kernel.ValueObjects.CreatedAt;
+using MovieFlow.Shared.Abstractions.Kernel.ValueObjects.UpdatedAt;
+using MovieFlow.Shared.Abstractions.Time;
 
 namespace MovieFlow.Modules.Movies.Infrastructure.EF.Context;
 
 internal class MoviesWriteDbContext : DbContext
 {
     public DbSet<Movie> Movies { get; set; }
+    private readonly IClock _clock;
     
-    public MoviesWriteDbContext(DbContextOptions<MoviesWriteDbContext> options) 
+    public MoviesWriteDbContext(DbContextOptions<MoviesWriteDbContext> options, IClock clock) 
         : base(options)
     {
+        _clock = clock;
     }
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("movies");
         modelBuilder.ApplyConfiguration(new MovieWriteConfiguration());
+    }
+    
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+    {
+        foreach (var entry in ChangeTracker.Entries<Movie>())
+        {
+            if (entry.State == EntityState.Added)
+                entry.Property<CreatedAt>("CreatedAt").CurrentValue = _clock.CurrentDateTimeOffset();
+            
+            if (entry.State == EntityState.Modified)
+                entry.Property<UpdatedAt>("UpdatedAt").CurrentValue = _clock.CurrentDateTimeOffset();
+
+        }
+        
+        return base.SaveChangesAsync(cancellationToken);
     }
 
 }
