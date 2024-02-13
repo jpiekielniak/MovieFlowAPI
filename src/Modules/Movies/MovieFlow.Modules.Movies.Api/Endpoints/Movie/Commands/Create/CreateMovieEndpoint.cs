3d8862c440a36522ee030ce3f96d9ee462c198bd
jpiekielniak <jakubpiekielniak123@gmail.com>
2024-02-13 14:15:40 +0100

    add: Authorize attribute

--- src/Modules/Movies/MovieFlow.Modules.Movies.Api/Endpoints/Movie/Commands/Create/CreateMovieEndpoint.cs
+++ src/Modules/Movies/MovieFlow.Modules.Movies.Api/Endpoints/Movie/Commands/Create/CreateMovieEndpoint.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using MovieFlow.Modules.Movies.Application.Movies.Commands.Create;
 
 namespace MovieFlow.Modules.Movies.Api.Endpoints.Movie.Commands.Create;
@@ -7,9 +8,11 @@ internal sealed class CreateMovieEndpoint(IMediator mediator) : EndpointBaseAsyn
     .WithRequest<CreateMovieCommand>
     .WithResult<CreateMovieResponse>
 {
+    [Authorize]
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [SwaggerOperation(
         Summary = "Create Movie",
         Tags = [MoviesEndpoint.Tag]
