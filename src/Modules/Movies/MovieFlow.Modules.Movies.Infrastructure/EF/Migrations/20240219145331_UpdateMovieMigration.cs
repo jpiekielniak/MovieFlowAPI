ea7816132f39b7277039fb2fe8078a25cb4694e3
jpiekielniak <jakubpiekielniak123@gmail.com>
2024-02-19 17:36:21 +0100

    add: update movie migration

---
+++ src/Modules/Movies/MovieFlow.Modules.Movies.Infrastructure/EF/Migrations/20240219145331_UpdateMovieMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieFlow.Modules.Movies.Infrastructure.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMovieMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                schema: "movies",
+                table: "Movies");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Rating",
+                schema: "movies",
+                table: "Movies",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
