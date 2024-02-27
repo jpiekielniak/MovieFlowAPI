768a0e1d4f0a617564700ee62564db0b74c18cd8
jpiekielniak <jakubpiekielniak123@gmail.com>
2024-02-27 16:39:41 +0100

    register: validators in extensions

--- src/Modules/Movies/MovieFlow.Modules.Movies.Application/Extensions.cs
+++ src/Modules/Movies/MovieFlow.Modules.Movies.Application/Extensions.cs
@@ -1,4 +1,7 @@
 using MovieFlow.Modules.Movies.Application.Genres.Commands.CreateGenre;
+using MovieFlow.Modules.Movies.Application.Movies.Commands.ChangeMovieInformation;
+using MovieFlow.Modules.Movies.Application.Reviews.Commands.AddReview;
+using MovieFlow.Modules.Movies.Application.Reviews.Commands.ChangeReviewInformation;
 using MovieFlow.Modules.Movies.Application.Services;
 using MovieFlow.Modules.Movies.Shared;
 
@@ -12,8 +15,14 @@ internal static class Extensions
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
-        
-        services.AddTransient<IValidator<CreateGenreCommand>, CreateGenreValidator>();
+
+        services
+            .AddScoped<IValidator<CreateGenreCommand>, CreateGenreValidator>()
+            .AddScoped<IValidator<CreateGenreCommand>, CreateGenreValidator>()
+            .AddScoped<IValidator<ChangeMovieInformationCommand>, ChangeMovieInformationValidator>()
+            .AddScoped<IValidator<AddReviewCommand>, AddReviewValidator>()
+            .AddScoped<IValidator<ChangeReviewInformationCommand>, ChangeReviewInformationValidator>();
+
         services.AddTransient<IMoviesModuleApi, MoviesModuleApi>();
 
         return services;
--- src/Modules/Users/MovieFlow.Modules.Users.Application/Extensions.cs
+++ src/Modules/Users/MovieFlow.Modules.Users.Application/Extensions.cs
@@ -1,3 +1,6 @@
+using MovieFlow.Modules.Users.Application.Users.Commands.ChangePassword;
+using MovieFlow.Modules.Users.Application.Users.Commands.SignIn;
+using MovieFlow.Modules.Users.Application.Users.Commands.SignUp;
 
 [assembly: InternalsVisibleTo("MovieFlow.Modules.Users.Api")]
 [assembly: InternalsVisibleTo("MovieFlow.Modules.Users.Infrastructure")]
@@ -9,6 +12,12 @@ internal static class Extensions
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(cfg=>cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+
+        services
+            .AddScoped<IValidator<ChangePasswordCommand>, ChangePasswordValidator>()
+            .AddScoped<IValidator<SignInCommand>, SignInValidator>()
+            .AddScoped<IValidator<SignUpCommand>, SignUpValidator>();
+        
         return services;
     }
 }
