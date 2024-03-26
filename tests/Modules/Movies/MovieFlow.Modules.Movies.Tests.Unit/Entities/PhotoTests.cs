7b9adcf00aa12944410e5796a879a2dfb0ab120f
jpiekielniak <jakubpiekielniak123@gmail.com>
2024-03-26 18:08:33 +0100

    add: photo entity tests

---
+++ tests/Modules/Movies/MovieFlow.Modules.Movies.Tests.Unit/Entities/PhotoTests.cs
@@ -0,0 +1,27 @@
+using MovieFlow.Modules.Movies.Core.Movies.Entities;
+using Shouldly;
+using Xunit;
+
+namespace MovieFlow.Modules.Movies.Tests.Unit.Entities;
+
+public class PhotoTests
+{
+    [Fact]
+    public void given_valid_photo_should_succeed()
+    {
+        //Arrange
+        const string fileName = "fileName";
+        const string url = "url";
+        const string alt = "alt";
+        const string contentType = "contentType";
+        
+        //Act
+        var photo = Photo.Create(fileName, url, alt, contentType);
+        
+        //Assert
+        photo.ShouldNotBeNull();
+        photo.ShouldBeOfType<Photo>();
+        photo.Id.ShouldBeAssignableTo<Guid>();
+        photo.Id.ShouldNotBe(Guid.Empty);
+    }
+}
